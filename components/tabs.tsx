e15b7de3b2b46377cbdb4e77502739156f810d55
culi <culitif@tuta.io>
2021-10-10 16:03:54 -0700

    create tabs component

---
+++ components/tabs.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import styles from './tabs.module.scss';
+
+interface Props {
+  children: React.ReactNode[];
+  tabNames: string[];
+};
+
+const Tabs = ({ children, tabNames }: Props) => {
+  const [activeTab, setActiveTab] = useState(0);
+  
+  return <div>
+    <ul role="tablist" className={styles.tabSwitcher}>
+      {tabNames.map((tabName, i) => <li key={tabName} role="presentation"> 
+        <a href="#" role="tab" aria-selected={activeTab === i} onClick={
+          e => {
+            e.preventDefault();
+            setActiveTab(i);
+          }
+        }>
+          {tabNames[i]}
+        </a>
+      </li>)}
+    </ul>
+
+    {children.map((tab, i) => i === activeTab && 
+      <div key={i} role="tabpanel" className={styles.tab}>
+        {tab}
+      </div>
+    )}
+  </div>;
+};
+
+export default Tabs;
